6178cd84ffd23bbaf40455057b1250e296075569
ashloh1991 <lohiaashish08@gmail.com>
2020-02-14 11:40:41 +0530

    solution

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number > 0)
+            {
+                Console.WriteLine($"The perfect digit is= {GetPerfectDigit(number)}");
+            }
+            else
+            {
+                Console.WriteLine($"Please enter a positive number");
+            }
+            Console.ReadKey();
+        }
+
+        public static int GetPerfectDigit(int digit)
+        {
+            int numToAppend = 10 - digit;
+
+            string result = Convert.ToString(digit) + Convert.ToString(numToAppend);
+
+            return Convert.ToInt32(result);
+        }
+    }
+}
